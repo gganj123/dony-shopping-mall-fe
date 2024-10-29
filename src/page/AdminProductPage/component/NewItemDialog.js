f378fb502e600ec2e5f499ffb5634f33c90f54f7
gganj123 <zwol10@naver.com>
2024-10-29 17:51:59 +0900

    study : handle submit 진행중 stock데이터 formdata안에 넣기:

--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -63,7 +63,9 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(FormData);
+    // setFormData(...formData, stock:{size,});
+    console.log("FormData", formData);
+    if (stock.length === 0) return setStockError(true);
     //재고를 입력했는지 확인, 아니면 에러
     // 재고를 배열에서 객체로 바꿔주기
     // [['M',2]] 에서 {M:2}로
