202c6014e07c14d95e72aafd177a179e54b50546
gganj123 <zwol10@naver.com>
2024-11-01 21:34:17 +0900

    fix : 헤더 배너 수정

--- src/common/component/EventHeader.js
+++ src/common/component/EventHeader.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../../App.css";
 
-const messages = ["어렵네요...이벤트 배너.."];
+const messages = ["코알누 구독 시 2천원 할인 쿠폰 증정!"];
 
 const EventHeader = () => {
   return (
@@ -10,7 +10,7 @@ const EventHeader = () => {
         <div
           key={index}
           className="message"
-          style={{ animationDelay: `${index * 5}s` }} // 각 메시지마다 애니메이션 지연 설정
+          style={{ animationDelay: `${index * 3}s` }} // 각 메시지마다 애니메이션 지연 설정
         >
           {message}
         </div>
--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -57,6 +57,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   }, [showDialog]);
 
   const handleClose = () => {
+    
     //모든걸 초기화시키고;
     // 다이얼로그 닫아주기
   };
