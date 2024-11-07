5d2019dbb81fba99a8f48c0702bd0d17caa11d82
gganj123 <zwol10@naver.com>
2024-11-07 14:03:51 +0900

    study : Google 소셜 로그인 기능 구현

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -7,6 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./style/login.style.css";
 import { loginWithEmail, loginWithGoogle } from "../../features/user/userSlice";
 import { clearErrors } from "../../features/user/userSlice";
+
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const Login = () => {
@@ -35,6 +36,7 @@ const Login = () => {
 
   const handleGoogleLogin = async (googleData) => {
     //구글 로그인 하기
+    console.log("googleDage:", googleData);
   };
 
   return (
@@ -84,6 +86,14 @@ const Login = () => {
           <div className="text-align-center mt-2">
             <p>Social Login</p>
             <div className="display-center">
+              {/* {구글 로그인 버튼 가져오기
+              2. Oauth 로그인을 우해서 googleapi사이트에  
+              가입하고 클라이언트키, 시크릿키 받아오기
+              3.로그인
+              4. 백엔드에서 로그인
+              a. 이미 로그인을 한유저 => 로그인시키고 토큰값 주면 장땡
+              b. 처음 로그인 시도를 한 유저다 => 유저정보 먼저 새로 생성=> 토큰값
+              } */}
               <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
                 <GoogleLogin
                   onSuccess={handleGoogleLogin}
