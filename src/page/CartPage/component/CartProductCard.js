1c55399c5fcdb48a049c4538ef7b3866fd59e24d
gganj123 <zwol10@naver.com>
2024-11-07 13:21:13 +0900

    study : cart item qty count 재고 있는만큼만 나오기

--- src/page/CartPage/component/CartProductCard.js
+++ src/page/CartPage/component/CartProductCard.js
@@ -15,6 +15,8 @@ const CartProductCard = ({ item }) => {
   const deleteCart = (id) => {
     dispatch(deleteCartItem(id));
   };
+  const stock = { ...item.productId.stock };
+  const stockCount = stock[item.size];
 
   return (
     <div className="product-card-cart">
@@ -49,7 +51,12 @@ const CartProductCard = ({ item }) => {
               defaultValue={item.qty}
               className="qty-dropdown"
             >
-              <option value={1}>1</option>
+              {[...Array(stockCount)].map((_, index) => (
+                <option key={index + 1} value={index + 1}>
+                  {index + 1}
+                </option>
+              ))}
+              {/* <option value={1}>1</option>
               <option value={2}>2</option>
               <option value={3}>3</option>
               <option value={4}>4</option>
@@ -58,7 +65,7 @@ const CartProductCard = ({ item }) => {
               <option value={7}>7</option>
               <option value={8}>8</option>
               <option value={9}>9</option>
-              <option value={10}>10</option>
+              <option value={10}>10</option> */}
             </Form.Select>
           </div>
         </Col>
